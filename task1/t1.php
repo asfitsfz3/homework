9902b6e5ae86888745183d4fd8b154fd4c198a7b
sergei <asfitsfz3@yandex.ru>
2018-08-17 14:00:50 +0300

    first

---
+++ task1/t1.php
@@ -0,0 +1,12 @@
+<pre>
+<?php
+
+
+$name = "Сергей";
+$age  = "28";
+
+echo "Меня зовут: ".$name;
+echo PHP_EOL;
+echo "Мне: ".$age." лет";
+echo PHP_EOL;
+echo "\"!|\\/'\"\\";
---
+++ test0/index.php
@@ -0,0 +1,8 @@
+<?php
+
+const TESTCOSTANTA = 'asd';
+$user_name = "Igor";
+
+if (1==1) {
+    echo 'hi';
+}
