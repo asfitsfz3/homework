22dbc32ce124f25017452f954eb2dab252d6f107
sergei <asfitsfz3@yandex.ru>
2018-08-21 00:43:45 +0300

    third

--- task1/t6.php
+++ task1/t6.php
@@ -7,11 +7,11 @@ for ($i=1; $i<=10; $i++) {
     for ($j=1; $j<=10; $j++) {
         echo "<td align='center'>";
         if (($i%2==0) and ($j%2==0)) {
-            echo "(".$i*$j.")";
+            echo $i." * ".$j." = (".$i*$j.")";
         } elseif (($i%2!=0) and ($j%2!=0)) {
-            echo "[".$i*$j."]";
+            echo $i." * ".$j." = [".$i*$j."]";
         } else {
-            echo $i*$j;
+            echo $i." * ".$j." = ".$i*$j;
         }
 
         echo "</td>";
