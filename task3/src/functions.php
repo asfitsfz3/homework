9a386e4ab6caedfe9ad7d68f781fb4b3db33c988
sergei <asfitsfz3@yandex.ru>
2018-08-24 11:24:07 +0300

    10

--- task3/src/functions.php
+++ task3/src/functions.php
@@ -1,16 +1,30 @@
 <?php
 function task1($filename)
 {
-    function parse_xml(SimpleXMLElement $element)
+
+    function parse_xml(SimpleXMLElement $element, int &$spaces)
     {
         foreach ($element->children() as $value) {
-            echo $value->getName().PHP_EOL;
-            parse_xml($value);
+            $spaces++;
+            for ($i=0; $i<=$spaces; $i++) {
+                echo "  ";
+            }
+            //echo " ".$spaces." ";
+            echo $value->getName();
+
+            if ($value->count()==0) {
+                echo ": ".$value->__toString() . PHP_EOL;
+            } else {
+                echo ": ".PHP_EOL;
+                parse_xml($value, $spaces);
+            }
+            $spaces--;
         }
     }
     if (file_exists($filename)) {
         $xml = new SimpleXMLElement($filename, false, true);
-        parse_xml($xml);
+        $sp=0;
+        parse_xml($xml, $sp);
         return 0;
     } else {
         return "Файл не существует";
