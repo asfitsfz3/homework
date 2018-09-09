9c90b5a3b3a73c9920379d15bb8d418a7626abfe
sergei <asfitsfz3@yandex.ru>
2018-09-09 17:01:26 +0300

    wp-3

--- vp3/wp-content/themes/turistik/single.php
+++ vp3/wp-content/themes/turistik/single.php
@@ -9,27 +9,32 @@
                 </div>
                 <div class="article-image"><?the_post_thumbnail();?></div>
                 <div class="article-info">
-                    <div class="post-date">29.07.2016</div>
+                    <div class="post-date"><?the_date();?></div>
                 </div>
                 <div class="article-text">
                     <?php the_content(); ?>
                 </div>
+                <?
+                $next_post = get_adjacent_post(0, '', 0);
+                $prev_post = get_adjacent_post();
+                ?>
                 <div class="article-pagination">
-                    <div class="article-pagination__block pagination-prev-left"><a href="#" class="article-pagination__link"><i class="icon icon-angle-double-left"></i>Предыдущая статья</a>
+                    <div class="article-pagination__block pagination-prev-left"><a href="<?echo get_permalink($prev_post->ID);?>" class="article-pagination__link"><i class="icon icon-angle-double-left"></i>Предыдущая статья</a>
                         <div class="wrap-pagination-preview pagination-prev-left">
-                            <div class="preview-article__img"><img src="img/1.jpg" class="preview-article__image"></div>
+                            <div class="preview-article__img"><img src="<?echo get_the_post_thumbnail_url($prev_post->ID);?>" class="preview-article__image"></div>
                             <div class="preview-article__content">
-                                <div class="preview-article__info"><a href="#" class="post-date">23.07.2016</a></div>
-                                <div class="preview-article__text">Сезонные скидки на авиа билеты в AirFace</div>
+                                <div class="preview-article__info"><a href="<?echo get_permalink($prev_post->ID);?>" class="post-date"><?echo $prev_post->post_date;?></a></div>
+                                <div class="preview-article__text"><?echo $prev_post->post_title;?></div>
                             </div>
                         </div>
                     </div>
-                    <div class="article-pagination__block pagination-prev-right"><a href="#" class="article-pagination__link">Сдедующая статья<i class="icon icon-angle-double-right"></i></a>
+
+                    <div class="article-pagination__block pagination-prev-right"><a href="<?echo get_permalink($next_post->ID);?>" class="article-pagination__link">Сдедующая статья<i class="icon icon-angle-double-right"></i></a>
                         <div class="wrap-pagination-preview pagination-prev-right">
-                            <div class="preview-article__img"><img src="img/2.jpg" class="preview-article__image"></div>
+                            <div class="preview-article__img"><img src="<?echo get_the_post_thumbnail_url($next_post->ID);?>" class="preview-article__image"></div>
                             <div class="preview-article__content">
-                                <div class="preview-article__info"><a href="#" class="post-date">23.07.2016</a></div>
-                                <div class="preview-article__text">Вылеты из Риги за 0,99 euro</div>
+                                <div class="preview-article__info"><a href="<?echo get_permalink($next_post->ID);?>" class="post-date"><?echo $next_post->post_date;?></a></div>
+                                <div class="preview-article__text"><?echo $next_post->post_title;?></div>
                             </div>
                         </div>
                     </div>
